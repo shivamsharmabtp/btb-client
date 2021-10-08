d7152ae76b7f2e3c80b699b297c29ac7552757c7
shivam <shivamsharma.btp@gmail.com>
2021-10-08 14:56:49 -0400

    sending build

---
+++ build/precache-manifest.5592982fd494142bf72bb309e5553375.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01acc30177c82d065d7503c0f22d5a82",
+    "url": "./index.html"
+  },
+  {
+    "revision": "062259f48d433cda1571",
+    "url": "./static/css/main.190e5440.chunk.css"
+  },
+  {
+    "revision": "650f1d531f57bd05837c",
+    "url": "./static/js/2.72e2cd6a.chunk.js"
+  },
+  {
+    "revision": "79a03052a393676e8de93c2221fb6af5",
+    "url": "./static/js/2.72e2cd6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "062259f48d433cda1571",
+    "url": "./static/js/main.a1a68962.chunk.js"
+  },
+  {
+    "revision": "0505a5a2fb9a1b9fca93",
+    "url": "./static/js/runtime-main.39193b6e.js"
+  },
+  {
+    "revision": "cfb49f55eb8da55ee5e66a7587b12849",
+    "url": "./static/media/empty.cfb49f55.svg"
+  },
+  {
+    "revision": "c4248ba0a3f23c42068a4c62bc0e6a35",
+    "url": "./static/media/profilePic.c4248ba0.jpeg"
+  }
+]);
